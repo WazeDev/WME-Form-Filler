7853db283836d0d53e6b9d9d9d6e3a1acb9bfb6e
Joe <crazycaveman@gmail.com>
2018-03-15 08:53:14 -0400

    Updated WME Form Filler.user.js
    Added WVMR form

--- WME Form Filler.user.js
+++ WME Form Filler.user.js
@@ -2,7 +2,7 @@
 // @name        WME Form Filler WVMR
 // @description Use info from WME to automatically fill out related forms - for the WV MapRaid
 // @namespace   https://greasyfork.org/users/6605
-// @version     1.3.7.1
+// @version     1.4.0
 // @homepage    https://github.com/WazeDev/WME-Form-Filler
 // @supportURL  https://github.com/WazeDev/WME-Form-Filler/issues
 // @include     https://www.waze.com/editor
@@ -532,115 +532,19 @@
             return;
         }
 
-        forms = [{
-                //https://docs.google.com/forms/d/e/1FAIpQLSduBiLMhbg6nRpsEVCTcVbV4eWmHDXdIKGtuaOvzy6NZLbSgw/viewform?entry.1553765347=username&entry.1264424583=CLOSED&entry.1811077109=permalink&entry.792657790=Two-Way&entry.345142186=reason&entry.1102521735=2016-09-20+03:00&entry.2015424420=street+name&entry.1547375393=from+street&entry.1335391716=to+street&entry.1867193205=SC&entry.1714138473=county&entry.1803937317=source&entry.1648634142=notes
-                name: "USA VEOC closures",
-                url: "https://docs.google.com/forms/d/e/1FAIpQLSduBiLMhbg6nRpsEVCTcVbV4eWmHDXdIKGtuaOvzy6NZLbSgw/viewform",
-                fields: {
-                    username: "1553765347",
-                    closureStatus: "1264424583",
-                    permalink: "1811077109",
-                    closedDir: "792657790",
-                    closedReason: "345142186",
-                    endDate: "1102521735",
-                    streetname: "2015424420",
-                    fromStreet: "1547375393",
-                    toStreet: "1335391716",
-                    stateabbr: "1867193205",
-                    county: "1714138473",
-                    source: "1803937317",
-                    notes: "1648634142"
-                }
-            }, {
-                name: "US Jane TTS Pronunciation",
-                url: "https://docs.google.com/forms/d/e/1FAIpQLSeuCmC0zy7GEQDJQP5R8dndxYhXCkqzadrPgP89BvatVl1bdg/viewform",
-                fields: {
-                    username: "324217272",
-                    state: "1065619417",
-                    issue: "1086951221",
-                    streetname: "1163516948",
-                    incorrectp: "1191620241",
-                    correctp: "1649051316",
-                    permalink: "2028167849",
-                    instructions: "2120232339",
-                    comments: "1917392591"
-                }
-            }, {
-                //https://docs.google.com/forms/d/e/1FAIpQLSff7nsBw8qxCojBdxrjTPl6tercqyyzGy92Vif_SBdHkYDchw/viewform?entry.1204781462=Reporter&entry.828228572=Reported&entry.1647952662=Street+name+&entry.1501712688=From+street+&entry.2094306654=To+street+&entry.1414240321=Two-Way&entry.900957975=10/27/2016+00:00&entry.1051351191=Adams&entry.1093044522=City+&entry.1540676081=IDOT&entry.430378754=Reason+&entry.1754051160=Permalink+&entry.172235277=Source+&entry.1722909714=Notes+
-                name: "Illinois event/weather closures",
-                url: "https://docs.google.com/forms/d/e/1FAIpQLSff7nsBw8qxCojBdxrjTPl6tercqyyzGy92Vif_SBdHkYDchw/viewform",
-                fields: {
-                    username: "1204781462",
-                    closureStatus: "828228572",
-                    permalink: "1754051160",
-                    closedDir: "1414240321",
-                    closedReason: "430378754",
-                    endDate: "900957975",
-                    streetname: "1647952662",
-                    fromStreet: "1501712688",
-                    toStreet: "2094306654",
-                    county: "1051351191",
-                    city: "1093044522",
-                    source: "172235277",
-                    notes: "1722909714"
-                }
-            }, {
-                //https://docs.google.com/forms/d/e/1FAIpQLSf3YAdjscVvJXiU4KWq0e8J8XyCyYBDyharRoXW3bf6R4wH1w/viewform?usp=pp_url&entry.1553765347=kwrigh01&entry.1264424583=CLOSED&entry.1811077109=https://www.waze.com/editor/?env%3Dusa%26lon%3D-80.26934%26lat%3D39.39069%26zoom%3D5%26segments%3D504613052,55526394,55530967&entry.792657790=Two-Way&entry.345142186=Road+Slip&entry.1102521735=2018-06-01+16:00&entry.2015424420=Janes+Hill+Rd&entry.1547375393&entry.1335391716&entry.1714138473=Harrison+&entry.1803937317=WVDOT+&entry.1648634142=Closed+Indefinitely
-                name: "WV closures",
-                url: "https://docs.google.com/forms/d/e/1FAIpQLSf3YAdjscVvJXiU4KWq0e8J8XyCyYBDyharRoXW3bf6R4wH1w/viewform",
-                fields: {
-                    username: "1553765347",
-                    closureStatus: "1264424583",
-                    permalink: "1811077109",
-                    closedDir: "792657790",
-                    closedReason: "345142186",
-                    endDate: "1102521735",
-                    streetname: "2015424420",
-                    fromStreet: "1547375393",
-                    toStreet: "1335391716",
-                    county: "1714138473",
-                    source: "1803937317",
-                    notes: "1648634142"
-                }
-            }, {
-                //https://docs.google.com/forms/d/e/1FAIpQLSeiKY0KsO0xN69Asw77MARQFmxOy6zQXF-k2OQdWOfwtiCp7Q/viewform?entry.1204781462=ojlaw&entry.828228572=CLOSED&entry.1647952662=Test1&entry.1501712688=Test2&entry.2094306654=Test3&entry.1414240321=One-Way&entry.900957975=00/00/0000+00:00&entry.1051351191=Adams&entry.1093044522=Test4&entry.1540676081=City&entry.430378754=Test5&entry.1754051160=Test6&entry.172235277=Test7&entry.1722909714=Test8
-                name: "Wisconsin event/weather closures",
-                url: "https://docs.google.com/forms/d/e/1FAIpQLSeiKY0KsO0xN69Asw77MARQFmxOy6zQXF-k2OQdWOfwtiCp7Q/viewform",
-                fields: {
-                    username: "1204781462",
-                    closureStatus: "828228572",
-                    permalink: "1754051160",
-                    closedDir: "1414240321",
-                    closedReason: "430378754",
-                    endDate: "900957975",
-                    streetname: "1647952662",
-                    fromStreet: "1501712688",
-                    toStreet: "2094306654",
-                    county: "1051351191",
-                    city: "1093044522",
-                    source: "172235277",
-                    notes: "1722909714"
-                }
+    forms = [
+        {
+            //https://docs.google.com/forms/d/e/1FAIpQLScQ429pP4qMAea7PKl6kaNfVG1ppYwNtBaunKGW18yAKFrJvA/viewform?usp=pp_url&entry.2005620554=kwrigh01&entry.1065046570=https://www.waze.com/editor/?env%3Dusa%26lon%3D-80.92719%26lat%3D38.04615%26zoom%3D4%26segments%3D81145965&entry.1166974658=Rename&entry.839337160=Wiki+Guidance
+            name: "WV Mapraid Segment Update request",
+            url: "https://docs.google.com/forms/d/e/1FAIpQLScQ429pP4qMAea7PKl6kaNfVG1ppYwNtBaunKGW18yAKFrJvA/viewform",
+            fields: {
+                username: "2005620554",
+                permalink: "1065046570",
+                requestType: "1166974658",
+                requestReason: "839337160"
             }
-            /*{
-            //https://docs.google.com/forms/d/e/1FAIpQLScY_5WKyYTqvH1fdiBThqLO4DRIzFzgdBtBexw5-iKL_LOzBw/viewform?entry.1553765347=username&entry.1264424583=CLOSED&entry.1811077109=permalink&entry.792657790=Two-Way&entry.345142186=reason&entry.1102521735=2016-09-20+03:00&entry.2015424420=street+name&entry.1547375393=from+street&entry.1335391716=to+street&entry.1867193205=SC&entry.1714138473=county&entry.1803937317=source&entry.1648634142=notes
-            name: "USA Weather related closures",
-            url: "https://docs.google.com/forms/d/e/1FAIpQLScY_5WKyYTqvH1fdiBThqLO4DRIzFzgdBtBexw5-iKL_LOzBw/viewform",
-            username: "1553765347",
-            closureStatus: "1264424583",
-            permalink: "1811077109",
-            closedDir: "792657790",
-            closedReason: "345142186",
-            endDate: "1102521735",
-            streetname: "2015424420",
-            fromStreet: "1547375393",
-            toStreet: "1335391716",
-            state: "1867193205",
-            county: "1714138473",
-            source: "1803937317",
-            notes: "1648634142",
-            }*/
-        ];
+        }
+    ];
 
         forms.forEach(function (key, i) {
             ffMnu.options.add(new Option(forms[i].name, i));
