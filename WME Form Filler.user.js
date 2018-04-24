182ec1331254386de9c3b98d7aed754474921757
crazycaveman <crazycaveman@gmail.com>
2018-04-24 15:44:43 -0400

    Prepare for release

--- WME Form Filler.user.js
+++ WME Form Filler.user.js
@@ -2,7 +2,7 @@
 // @name        WME Form Filler
 // @description Use info from WME to automatically fill out related forms
 // @namespace   https://greasyfork.org/users/6605
-// @version     1.4.0b6
+// @version     1.4.0
 // @homepage    https://github.com/WazeDev/WME-Form-Filler
 // @supportURL  https://github.com/WazeDev/WME-Form-Filler/issues
 // @include     https://www.waze.com/editor
